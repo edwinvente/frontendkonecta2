23caadbe6449bee9ffcf2d09e2bf758a263697e6
Creandola <comercial@somoscreandola.com>
2021-02-19 20:52:08 -0500

    order app component

--- src/App.js
+++ src/App.js
@@ -3,53 +3,30 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
   useHistory,
   useLocation
 } from "react-router-dom";
 
-// This example has 3 pages: a public page, a protected
-// page, and a login screen. In order to see the protected
-// page, you must first login. Pretty standard stuff.
-//
-// First, visit the public page. Then, visit the protected
-// page. You're not yet logged in, so you are redirected
-// to the login page. After you login, you are redirected
-// back to the protected page.
-//
-// Notice the URL change each time. If you click the back
-// button at this point, would you expect to go back to the
-// login page? No! You're already logged in. Try it out,
-// and you'll see you go back to the page you visited
-// just *before* logging in, the public page.
+import { Navbar } from "./components/index";
+import { Blog } from "./blog";
 
-export default function AuthExample() {
+const urlApi = 'http://localhost:8000/api/';
+
+const AuthExample = () => {
   return (
     <ProvideAuth>
       <Router>
         <div>
+          <Navbar />
           <AuthButton />
 
-          <ul>
-            <li>
-              <Link to="/public">Public Page</Link>
-            </li>
-            <li>
-              <Link to="/protected">Protected Page</Link>
-            </li>
-          </ul>
-
           <Switch>
-            <Route path="/public">
-              <PublicPage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <PrivateRoute path="/protected">
-              <ProtectedPage />
-            </PrivateRoute>
+            <Route path="/" children={<HomePage />} exact />
+            <Route path="/login" children={<LoginPage />} />
+            {/* rutas protegidas */}
+            <PrivateRoute path="/protected" children={<ProtectedPage />} />
+            <PrivateRoute path="/blog" children={<Blog />} />
           </Switch>
         </div>
       </Router>
@@ -69,10 +46,6 @@ const fakeAuth = {
   }
 };
 
-/** For more details on
- * `authContext`, `ProvideAuth`, `useAuth` and `useProvideAuth`
- * refer to: https://usehooks.com/useAuth/
- */
 const authContext = createContext();
 
 function ProvideAuth({ children }) {
@@ -117,23 +90,25 @@ function AuthButton() {
   let auth = useAuth();
 
   return auth.user ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          auth.signout(() => history.push("/"));
-        }}
-      >
-        Sign out
+    <div className='container-fluid'>
+      <p>
+        Welcome!{" "}
+        <button
+          onClick={() => {
+            auth.signout(() => history.push("/"));
+          }}
+        >
+          Sign out
       </button>
-    </p>
+      </p>
+    </div>
   ) : (
-      <p>You are not logged in.</p>
+      <div className='container-fluid'>
+        <p>You are not logged in.</p>
+      </div>
     );
 }
 
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
 function PrivateRoute({ children, ...rest }) {
   let auth = useAuth();
   return (
@@ -155,30 +130,71 @@ function PrivateRoute({ children, ...rest }) {
   );
 }
 
-function PublicPage() {
-  return <h3>Public</h3>;
+function HomePage() {
+  return <h3>Home</h3>;
 }
 
 function ProtectedPage() {
   return <h3>Protected</h3>;
 }
 
+function BlogPage() {
+  return <h3>Blog</h3>;
+}
+
 function LoginPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  let status = false;
   let history = useHistory();
   let location = useLocation();
   let auth = useAuth();
 
   let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    auth.signin(() => {
-      history.replace(from);
+
+  let login = async () => {
+
+    const request = await fetch(urlApi + 'login', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        "email": username,
+        "password": password
+      })
     });
+
+    const data = await request.json();
+    await accessDashboard(data.token)
+
   };
 
+  const accessDashboard = (token) => {
+    window.localStorage.setItem("token", JSON.stringify(token))
+    if (token) {
+      auth.signin(() => {
+        history.replace(from);
+      });
+    }
+  }
+
   return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-3'>
+          <p>Necesitas loguearte para acceder al {from.pathname}</p>
+
+          <input type='text' className='form-control' placeholder='username' onChange={(e) => setUsername(e.target.value)} /><br />
+          <input type='password' className='form-control' placeholder='password' onChange={(e) => setPassword(e.target.value)} /> <br />
+          <button className='btn btn-primary btn-sm btn-block' onClick={login}>Ingresar</button>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default AuthExample
