90c1a5934f9d69e736f9ce44cb070fe89924e902
Creandola <comercial@somoscreandola.com>
2021-02-19 20:53:11 -0500

    start project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthExample from './App';
+import index from './Index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
