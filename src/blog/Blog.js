a78af3bcb6d67da77c7ea48e8250a40ba9a49c2d
Creandola <comercial@somoscreandola.com>
2021-02-19 20:53:02 -0500

    blog component

---
+++ src/blog/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Blog = () => {
+    return (
+        <div className='container-fluid'>
+            <h1 className='text-center m-4'>Blog</h1>
+            <div className='row'>
+                <div className='col-md-8'>
+                    Content
+                </div>
+                <div className='col-md-4'>
+                    Links
+                </div>
+            </div>
+        </div>
+    )
+}
