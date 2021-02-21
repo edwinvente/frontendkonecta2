7cab9f87a842e49d6ecf524ea52af3aa5d1ea14d
Creandola <comercial@somoscreandola.com>
2021-02-21 18:27:28 -0500

    Home page include styles bootstrap success

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <div className='container-flud'>
+            <h1 className='text-center'>Prueba de desarrollo FullStack React & Laravel</h1> <hr />
+            <div className='container'>
+                <p>
+                    Nuestro blog utilizando herramientas como: JWT, React, ECMC6, Laravel, Mysql...
+                </p>
+            </div>
+        </div>
+    )
+}
