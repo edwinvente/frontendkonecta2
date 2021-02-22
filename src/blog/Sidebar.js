906b358ee071b3401455b26d2d4e4b8cf5c941d9
Creandola <comercial@somoscreandola.com>
2021-02-22 01:33:12 -0500

    sidebar navigation

--- src/blog/Sidebar.js
+++ src/blog/Sidebar.js
@@ -5,12 +5,12 @@ export const Sidebar = () => {
     return (
         <div>
             <ul className=''>
-                <li className='nav-link'><Link to="/perfil">Mi perfil</Link></li>
-                <li className='nav-link'><Link to="/blog">Blog</Link></li>
-                <li className='nav-link'><Link to="/crear/post">Crear Post</Link></li>
-                <li className='nav-link'><Link to="/categorias">Categorias</Link></li>
-                <li className='nav-link'><Link to="/crear/categoria">Crear Categoria</Link></li>
-                <li className='nav-link'><Link to="/favoritos">Mis Favoritos</Link></li>
+                <li className='nav-link'><Link to="/perfil">Mi perfil 📁</Link></li>
+                <li className='nav-link'><Link to="/blog">Blog 📁</Link></li>
+                <li className='nav-link'><Link to="/crear/post">Crear Post 📁</Link></li>
+                <li className='nav-link'><Link to="/categorias">Categorias 📁</Link></li>
+                <li className='nav-link'><Link to="/crear/categoria">Crear Categoria 📁</Link></li>
+                {/* <li className='nav-link'><Link to="/favoritos">Mis Favoritos</Link></li> */}
             </ul>
         </div>
     )
