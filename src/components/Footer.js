fd3854b81db566d60176800893d9f79b5798fd27
Creandola <comercial@somoscreandola.com>
2021-02-22 01:32:38 -0500

    footer app

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+
+export const Footer = () => {
+    return (
+        <footer className="footer py-3 bg-light">
+            <div className="container">
+                <span className="text-muted">Desarrollado con ❤ por Edwin Caicedo Venté.</span>
+            </div>
+        </footer>
+    )
+}
